464287896acaeaa51011af3d20120f31bdfcaee0
Gabor Szarnyas <szarnyasg@gmail.com>
2024-01-05 14:57:23 +0100

    Add workflow

---
+++ scripts/check-issue-for-code-formatting.py
@@ -0,0 +1,13 @@
+import re
+import sys
+
+post = sys.stdin.read()
+
+re.compile(r'')
+sql_keywords = len(re.findall(r"(select|from|where) ", post, flags=re.IGNORECASE))
+backticked_code_blocks = len(re.findall(r"^```", post))
+inline_code_snippets = len(re.findall(r"`", post)) // 2
+indented_code_lines = len(re.findall(r"^    (SELECT|FROM|import|duckdb|library)", post))
+
+if backticked_code_blocks == 0 and backticked_code_blocks == 0 and inline_code_snippets == 0 and sql_keywords > 0:
+    exit(1)
