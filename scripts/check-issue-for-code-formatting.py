5a700894a9e637e233b1e779322a126d80c6e0a0
Gabor Szarnyas <szarnyasg@gmail.com>
2024-01-05 15:39:03 +0100

    duh

--- scripts/check-issue-for-code-formatting.py
+++ scripts/check-issue-for-code-formatting.py
@@ -9,5 +9,7 @@ backticked_code_blocks = len(re.findall(r"^```", post))
 inline_code_snippets = len(re.findall(r"`", post)) // 2
 indented_code_lines = len(re.findall(r"^    (SELECT|FROM|import|duckdb|library)", post))
 
+print(f"sql keywords: {sql_keywords}")
+
 if backticked_code_blocks == 0 and backticked_code_blocks == 0 and inline_code_snippets == 0 and sql_keywords > 0:
     exit(1)
